ef968fd6572f59b496a2fd927e34864c6dcf4d84
DeeeMoss <123mosich123@mail.ru>
2023-09-01 21:49:31 +0500

    Добавил возможность сохранять данные из инпутов

---
+++ app.js
@@ -0,0 +1,9 @@
+function sendMeMessage() {
+    const inputName = document.getElementById('name')
+    const inputEmail = document.getElementById('email')
+    const inputArea = document.getElementById('text')
+    let name = inputName.value
+    let email = inputEmail.value
+    let message = inputArea.value
+    let fullMessage = `Имя: ${name}\nemail: ${email}\nСообщение: ${message}`
+}
