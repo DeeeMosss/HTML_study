7209e0e3a24c03c8267b6dd30cc3ae43ab085f64
DeeeMoss <123mosich123@mail.ru>
2023-09-04 01:33:36 +0500

    Для Миши

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+
 function sendMeMessage() {
-    const inputName = document.getElementById('name')
-    const inputEmail = document.getElementById('email')
-    const inputArea = document.getElementById('text')
-    let name = inputName.value
-    let email = inputEmail.value
-    let message = inputArea.value
-    let fullMessage = `Имя: ${name}\nemail: ${email}\nСообщение: ${message}`
+  const inputName = document.getElementById('name');
+  const inputEmail = document.getElementById('email');
+  const inputArea = document.getElementById('text');
+  let name = inputName.value;
+  let email = inputEmail.value;
+  let message = inputArea.value;
+  let fullMessage = `Имя: ${name}\nemail: ${email}\nСообщение: ${message}`;
+  return fullMessage;
 }
